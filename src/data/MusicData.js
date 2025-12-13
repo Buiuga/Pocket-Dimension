a988ca604031f7bd6ec3e71f23fdb551bb5e20e7
Iulian <burciuiuliangabriel18@gmail.com>
2025-12-13 15:42:52 +0200

    add starry sky music on deploy

--- src/data/MusicData.js
+++ src/data/MusicData.js
@@ -1,42 +1,42 @@
 export const PLAYLIST = [
   {
     title: "In The Sea",
-    url: "../../Public/Playlist/in the sea.mp3",
+    url: "/Pocket-Dimension/Playlist/in the sea.mp3",
   },
   {
     title: "Attack Guitar 2013",
-    url: "../../Public/Playlist/AotOST.mp3",
+    url: "/Pocket-Dimension/Playlist/AotOST.mp3",
   },
   {
     title: "Watching the Stars",
-    url: "../../Public/Playlist/watching the stars.mp3",
+    url: "/Pocket-Dimension/Playlist/watching the stars.mp3",
   },
   {
     title: "Cornfield Chase",
-    url: "../../Public/Playlist/Cornfield chase.mp3",
+    url: "/Pocket-Dimension/Playlist/Cornfield chase.mp3",
   },
   {
     title: "Stellar",
-    url: "../../Public/Playlist/stellar.mp3",
+    url: "/Pocket-Dimension/Playlist/stellar.mp3",
   },
   {
     title: "Lagoon",
-    url: "../../Public/Playlist/lagoon.mp3",
+    url: "/Pocket-Dimension/Playlist/lagoon.mp3",
   },
   {
     title: "September",
-    url: "../../Public/Playlist/September.mp3",
+    url: "/Pocket-Dimension/Playlist/September.mp3",
   },
   {
     title: "Children",
-    url: "../../Public/Playlist/Children.mp3",
+    url: "/Pocket-Dimension/Playlist/Children.mp3",
   },
   {
     title: "Icarus",
-    url: "../../Public/Playlist/Icarus.mp3",
+    url: "/Pocket-Dimension/Playlist/Icarus.mp3",
   },
   {
     title: "Small Village",
-    url: "../../Public/Playlist/Small village.mp3",
+    url: "/Pocket-Dimension/Playlist/Small village.mp3",
   },
 ];
